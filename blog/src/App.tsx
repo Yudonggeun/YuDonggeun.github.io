e1db2ae2474c8be8dcac9800d3d7d6dfff089e2f
yudonggeun <ydong98@gmail.com>
2024-08-21 12:23:27 +0900

    zustand 페이지 이동 상태 구현

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -6,11 +6,38 @@ import AuctionList from './pages/AuctionList';
 import LoginPage from './pages/Login';
 import ChargePointPage from './pages/ChargePoint';
 import ReceiptListPage from './pages/ReceiptList';
+import {usePageStore} from "./store/PageStore";
+import Footer from "./pages/Footer";
 
 function App() {
+
+  const { currentPage, setPage } = usePageStore();
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <AuctionList />;
+      case 'auctionDetail':
+        return <AuctionDetails />;
+      case 'login':
+        return <LoginPage />;
+      case 'charge':
+        return <ChargePointPage />;
+      case 'receiptList':
+        return <ReceiptListPage />;
+    }
+  }
+
   return (
-    <AuctionDetails />
-    // <AuctionList />
+      <>
+        <div>
+          {renderPage()}
+        </div>
+        <Footer />
+      </>
+
+      // <AuctionDetails />
+      // <AuctionList />
     // <LoginPage />
     // <ChargePointPage />
     // <ReceiptListPage />
